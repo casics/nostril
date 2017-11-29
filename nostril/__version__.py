7e4494d269d5bcb615202b6f71f7e456fadaa879
Michael Hucka <mhucka@caltech.edu>
2017-11-29 14:29:45 -0800

    Add a typical version info

---
+++ nostril/__version__.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Nostril version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/nostril
+# =============================================================================
+
+__version__   = '1.0.0'
+__title__     = 'Nostril: Nonsense String Evaluator'
+__url__       = 'https://github.com/casics/nostril'
+__author__    = 'Michael Hucka <mhucka@caltech.edu>'
+__email__     = 'casics-team@googlegroups.com'
+__license__   = 'GNU General Public License, version 3.0'
+__copyright__ = 'Copyright (C) 2015-2017 by the California Institute of Technology'
