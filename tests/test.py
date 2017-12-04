3222cfb2d93888fc1275159301876f8bcb206d8a
Michael Hucka <mhucka@caltech.edu>
2017-12-03 18:16:14 -0800

    Update expected scores based on recent changes to algorithm

--- tests/test.py
+++ tests/test.py
@@ -7,8 +7,6 @@ from   time import time
 
 thisdir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(thisdir, '..'))
-sys.path.append(os.path.join(thisdir, '../..'))
-sys.path.append(os.path.join(thisdir, '../../common'))
 
 from nostril import *
 
@@ -24,33 +22,35 @@ assert is_nonsense('iuewrofahgalkfgaufpiupqrjf')
 assert is_nonsense('ieeoienkjadfakj')
 assert is_nonsense('lalalaalkjuogaajfajlfal')
 
+is_nonsense = generate_nonsense_detector()
+
 print('')
-print('Testing all words in /usr/share/dict/web2 -- expect 21 failures:')
+print('Testing all words in /usr/share/dict/web2 -- expect 22 failures (99.99% correct):')
 test_strings('/usr/share/dict/web2', is_nonsense, trace_scores=True)
 
 print('')
-print('Testing against Loyola cases -- expect 1 failure:')
+print('Testing against Loyola cases -- expect 2 failures:')
 test_strings('unlabeled-cases/loyola-u-ids-cleaned.txt', is_nonsense, trace_scores=True) 
 
 print('')
-print('Testing real-not-real -- expect 3 false positives, 18 false negatives:')
+print('Testing real-not-real -- expect 5 false positives, 9 false negatives:')
 result = test_labeled('labeled-cases/real-not-real.csv', is_nonsense, trace_scores=True)
 
 print('')
-print('Testing against real OSX identifiers -- expect 4 failures:')
+print('Testing against real OSX identifiers -- expect 5 failures:')
 test_strings('unlabeled-cases/select-identifiers-from-osx-frameworks.txt', is_nonsense, trace_scores=True)
 
 print('')
-print('Testing against hand-written random strings -- expect 67% correct:')
+print('Testing against hand-written random strings -- expect 70% correct:')
 test_strings('unlabeled-cases/random-by-hand.txt', is_nonsense, sense='invalid', trace_scores=True) 
 
 print('')
-print('Recall test: all of these are believed to be real -- expect 3 failures:') 
-(failures, successes, count, skipped, elapsed_time) = test_strings('../training/identifier-corpora/random-identifiers-from-github.txt', is_nonsense, trace_scores=True)
+print('Recall test: all of these are believed to be real -- expect 6 failures:')
+(failures, successes, count, skipped, elapsed_time) = test_strings('../nostril/training/identifier-corpora/random-identifiers-from-github.txt', is_nonsense, trace_scores=True)
 print('Recall = {:.6f}'.format(successes/count))
 
 print('')
-print('Recall test: all of these are truly random strings:') 
+print('Recall test: all of these are truly random strings -- expect 85.75% correct:')
 random_strings = dataset_from_pickle('unlabeled-cases/random_set.pklz')
 (failures, successes, count, skipped, elapsed_time) = test_strings(random_strings[:1000000], is_nonsense, trace_scores=True, sense='nonsense')
 print('Recall = {:.6f}'.format(successes/count))
